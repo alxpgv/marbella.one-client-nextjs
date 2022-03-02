42296182b5a55ce8e673387a37d4f0e12a048f26
alxpgv <alex@ipcoder.ru>
2022-03-03 02:19:58 +0300

    upd

--- src/components/blocks/ListService/ListService.tsx
+++ src/components/blocks/ListService/ListService.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@/components/Button";
 
 // TODO: adding typing items
 const ListService = ({ items }: { items: any[] }) => {
@@ -8,10 +9,14 @@ const ListService = ({ items }: { items: any[] }) => {
         items.map((item, i) => {
           return (
             <div
-              className="flex-1 min-w-[200px] w-[300px] h-[300px] border border-red-400"
+              className="flex-1 min-w-[200px] w-[300px] h-[300px] bg-white/90 border border-red-400"
               key={i}
             >
-              {item.title}
+              <h6>{item.title}</h6>
+              <p>{item.text}</p>
+              <Button size="sm" color="red">
+                Read more
+              </Button>
             </div>
           );
         })}
