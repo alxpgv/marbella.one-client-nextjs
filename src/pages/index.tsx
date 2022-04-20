5a3753210c6407886b5e4fbe15f3d2a6c77176b1
alxpgv <alex@ipcoder.ru>
2022-04-20 10:35:00 +0300

    add block dream perfect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import { NextPage } from "next";
 import { HomePromo } from "@/components/blocks/HomePromo";
 import { MainLayout } from "@/layout/MainLayout";
 import { ListWithImageText } from "@/components/blocks/ListWithImageText";
+import { ImageWithText } from "@/components/blocks/ImageWithText";
 import { home } from "@/data/pages/home";
 
 const Index: NextPage = () => {
   return (
     <MainLayout>
       {home?.promo && <HomePromo {...home.promo} />}
-      {home?.listIdealHome && <ListWithImageText {...home.listIdealHome} />}
+      {home?.listIdeal && <ListWithImageText {...home.listIdeal} />}
+      {home?.dreamPerfect && <ImageWithText {...home.dreamPerfect} />}
     </MainLayout>
   );
 };
