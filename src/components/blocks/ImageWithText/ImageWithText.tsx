288ffeceb582704ba0b97b3852af3fec4f623e06
alxpgv <alex@ipcoder.ru>
2022-04-20 10:34:10 +0300

    create block

---
+++ src/components/blocks/ImageWithText/ImageWithText.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from "react";
+import { ImageProps } from "@/types";
+import { Section } from "@/components/Section";
+import { Container } from "@/components/Container";
+import Image from "next/image";
+import cn from "classnames";
+import styles from "./ImageWithText.module.scss";
+import { Button } from "@/components/Button";
+import { IconArrow } from "@/components/Icons";
+
+interface ImageWithTextProps {
+  title?: string;
+  text?: string;
+  image: ImageProps;
+}
+
+export const ImageWithText: FC<ImageWithTextProps> = ({
+  title,
+  text,
+  image,
+}) => {
+  return (
+    <Section>
+      <Container className={styles.wrapper}>
+        {image?.url && (
+          <div className={styles.image}>
+            <Image
+              src={image.url}
+              layout="fill"
+              objectFit="cover"
+              alt={image.alt ?? ""}
+            />
+          </div>
+        )}
+        <div className={styles.content}>
+          {title && (
+            <div className={styles.contentHeading}>
+              <h2 className="text-lg-10">{title}</h2>
+            </div>
+          )}
+          {text && (
+            <div
+              className={cn("text-sm-30", styles.contentText)}
+              dangerouslySetInnerHTML={{ __html: text }}
+            />
+          )}
+          <div className={styles.btn}>
+            <Button
+              size={"md"}
+              variant={"primary"}
+              icon={<IconArrow rotate={"-rotate-90"} />}
+            >
+              Learn more
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </Section>
+  );
+};
