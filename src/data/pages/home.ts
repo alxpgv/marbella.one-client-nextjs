bf4e85914e52c3d532cf52b60649e570094b8246
alxpgv <alex@ipcoder.ru>
2022-04-20 10:33:51 +0300

    add block

--- src/data/pages/home.ts
+++ src/data/pages/home.ts
@@ -12,7 +12,7 @@ export const home = {
     },
     services: serviceItems,
   },
-  listIdealHome: {
+  listIdeal: {
     text: "An ideal home is your personal fortress, in which it is safe and good in any weather in the shower and outside the window. Here is everything that was dreamed of in childhood. Every detail blends harmoniously with the interior and creates an environment with your character and your interests.",
     listItems: [
       "Home is not just a place where the whole family gathers in the evenings for a cup of tea.",
@@ -25,5 +25,16 @@ export const home = {
       width: 740,
       height: 257,
     },
+  },
+  dreamPerfect: {
+    image: {
+      url: "/images/home/ideal-home-min.jpg",
+      alt: "Dream of a perfect home",
+      width: 760,
+      height: 589,
+    },
+    title:
+      "Do you already know what your ideal home will be? Draw it in your imagination?",
+    text: "<p>Our real estate agency Violet Real Estate will help you make your dream of a perfect home come true!</p><p>We understand your desire to cross the threshold of such a house one day, and therefore we will do our best to make it a reality!</p>",
   },
 };
