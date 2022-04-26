4a3d5920aebfcc2b6237e26a5084df615f11e760
alxpgv <alex@ipcoder.ru>
2022-04-26 03:31:33 +0300

    online service data

--- src/data/pages/blocks/online-services.ts
+++ src/data/pages/blocks/online-services.ts
@@ -2,11 +2,23 @@ export const onlineServices = [
   {
     id: 1,
     title: "Yacht charter",
+    image: {
+      url: "/images/home/yacht-charter-min.jpg",
+      width: 300,
+      height: 440,
+      alt: "Yacht charter",
+    },
     link: "/#",
   },
   {
     id: 2,
     title: "Private jet charter",
+    image: {
+      url: "/images/home/private-jet-charter-min.jpg",
+      width: 300,
+      height: 440,
+      alt: "Private jet charter",
+    },
     link: "/#",
   },
 ];
