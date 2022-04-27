19c31376728f0adefce70e53a3583ea753fa1ace
alxpgv <alex@ipcoder.ru>
2022-04-28 00:19:14 +0300

    refactoring

--- src/data/static-blocks/index.ts
+++ src/data/static-blocks/index.ts
@@ -1,6 +1,6 @@
 import { banners } from "./banners";
 import { onlineServices } from "./online-services";
 import { ourServices } from "./our-services";
-import { onlineConsultation } from "./onlineConsultation";
+import { onlineConsultation } from "./online-consultation";
 
 export { banners, onlineServices, ourServices, onlineConsultation };
