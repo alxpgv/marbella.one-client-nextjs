2febfd54f0b182719db2b6cdf26cab3709ea2a48
alxpgv <alex@ipcoder.ru>
2022-05-10 23:15:49 +0300

    upd

--- src/blocks/null-block/null-block.tsx
+++ src/blocks/null-block/null-block.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./null-block.module.scss";
 import cn from "classnames";
 
-export const NullBlock = ({ blockName = "" }: { blockName: string }) => {
+export const NullBlock = ({ blockName }: { blockName: string }) => {
   return (
     <div className={cn(styles.block, "text-sm")}>
       Not found component by name: <strong>{blockName}</strong>
