90e6540d0bc49af8d511007c2ca098d5bd591b5b
alxpgv <alex@ipcoder.ru>
2022-05-12 01:17:03 +0300

    add size image

--- src/layout/logo/logo.tsx
+++ src/layout/logo/logo.tsx
@@ -11,9 +11,19 @@ export const Logo: FC<LogoProps> = ({ className = "", color = "primary" }) => {
     <Link href="/">
       <a className={className}>
         {color === "primary" ? (
-          <img src="/images/logo.svg" alt="Violet Real Estate" />
+          <img
+            src="/images/logo.svg"
+            width="138"
+            height="47"
+            alt="Violet Real Estate"
+          />
         ) : (
-          <img src="/images/logo-secondary.svg" alt="Violet Real Estate" />
+          <img
+            src="/images/logo-secondary.svg"
+            width="138"
+            height="47"
+            alt="Violet Real Estate"
+          />
         )}
       </a>
     </Link>
