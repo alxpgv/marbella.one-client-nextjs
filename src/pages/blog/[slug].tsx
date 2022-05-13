b1413782c997387a68cf53d08d74802322d6c541
alxpgv <alex@ipcoder.ru>
2022-05-13 20:08:24 +0300

    create

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NextPage } from "next";
+import { MainLayout } from "@/layout/main-layout";
+import { Meta } from "@/layout/meta";
+import { Container } from "@/components/container";
+import { Breadcrumb } from "@/components/breadcrumb";
+import { PageTitle } from "@/components/page-title";
+
+const BlogPage: NextPage = () => {
+  return (
+    <MainLayout>
+      <Container>
+        <Breadcrumb />
+        <PageTitle title={"Blog"} />
+      </Container>
+    </MainLayout>
+  );
+};
+
+export default BlogPage;
