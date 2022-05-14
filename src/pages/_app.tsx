b51fc033708d23c7db639301e3c85c0907ac7b2b
alxpgv <alex@ipcoder.ru>
2022-05-15 00:47:33 +0300

    upd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/styles.scss";
 import type { AppProps } from "next/app";
-import { Head } from "@/components/common/Head";
+import { Head } from "@/components/common/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
