22445679fdda155fbd9347afb2973e4919f0f8ac
alxpgv <alex@ipcoder.ru>
2022-05-15 04:23:29 +0300

    upd

---
+++ src/components/blog/blog-category/blog-category-entry.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from "react";
+import type { EntryProps } from "@/types/entry";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import cn from "classnames";
+import styles from "./blog-category.module.scss";
+
+export const BlogCategoryEntry: FC<EntryProps> = ({
+  title,
+  slug,
+  description,
+  image,
+}) => {
+  const href = `/blog/${slug}`;
+  return (
+    <article className={styles.entry}>
+      {image?.src && (
+        <Link href={href}>
+          <a className={styles.imageWrapper}>
+            <Image
+              src={image.src}
+              layout="fill"
+              objectFit="cover"
+              alt={image.alt ?? ""}
+            />
+          </a>
+        </Link>
+      )}
+      <div className={styles.content}>
+        {title && (
+          <Link href={href}>
+            <a className={styles.titleLink}>
+              <h4 className={styles.title}>{title}</h4>
+            </a>
+          </Link>
+        )}
+        {description && (
+          <p className={cn(styles.description, "text-grey-300")}>
+            {description}
+          </p>
+        )}
+        <div className={styles.btn}>
+          <Button variant={"four"} size={"sm"} as={"link"} href={href} />
+        </div>
+      </div>
+    </article>
+  );
+};
--- src/components/blog/blog-category/blog-category.tsx
+++ src/components/blog/blog-category/blog-category.tsx
@@ -2,11 +2,8 @@ import React, { FC } from "react";
 import type { EntryProps } from "@/types/entry";
 import { Container } from "@/components/ui/container";
 import { Section } from "@/components/ui/section";
-import Link from "next/link";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { BlogCategoryEntry } from "./blog-category-entry";
 import styles from "./blog-category.module.scss";
-import cn from "classnames";
 
 export interface BlogProps {
   entries: EntryProps[];
@@ -18,47 +15,9 @@ export const BlogCategory: FC<BlogProps> = ({ entries = [] }) => {
       <Container>
         <article className={styles.wrapper}>
           {entries.length > 0
-            ? entries.map(({ id, title, slug, description, image }) => {
-                const href = `/blog/${slug}`;
-                return (
-                  <div key={id} className={styles.entry}>
-                    {image?.src && (
-                      <Link href={href}>
-                        <a className={styles.imageWrapper}>
-                          <Image
-                            src={image.src}
-                            layout="fill"
-                            objectFit="cover"
-                            alt={image.alt ?? ""}
-                          />
-                        </a>
-                      </Link>
-                    )}
-                    <div className={styles.content}>
-                      {title && (
-                        <Link href={href}>
-                          <a className={styles.titleLink}>
-                            <h4 className={styles.title}>{title}</h4>
-                          </a>
-                        </Link>
-                      )}
-                      {description && (
-                        <p className={cn(styles.description, "text-grey-300")}>
-                          {description}
-                        </p>
-                      )}
-                      <div className={styles.btn}>
-                        <Button
-                          variant={"four"}
-                          size={"sm"}
-                          as={"link"}
-                          href={href}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                );
-              })
+            ? entries.map((entry) => (
+                <BlogCategoryEntry key={entry.slug} {...entry} />
+              ))
             : `No entries found`}
         </article>
       </Container>
