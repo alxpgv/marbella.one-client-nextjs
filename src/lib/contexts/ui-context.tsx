68a5ed066d3bdb87680ba9e6b67bb57529686f52
alxpgv <alex@ipcoder.ru>
2022-05-20 12:47:22 +0300

    create UIContext

---
+++ src/lib/contexts/ui-context.tsx
@@ -0,0 +1,77 @@
+import React, {
+  FC,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useReducer,
+} from "react";
+
+interface UIState {
+  displayModal: boolean;
+  modalView: string;
+}
+
+type MODAL_VIEWS = "CALLBACK" | "SUBSCRIBE" | "CONSULTATION";
+
+type Action =
+  | { type: "OPEN_MODAL" }
+  | { type: "CLOSE_MODAL" }
+  | { type: "SET_MODAL_VIEW"; view: MODAL_VIEWS };
+
+const initialState = {
+  displayModal: false,
+  modalView: "CALLBACK",
+};
+
+const UIContext = createContext<UIState | any>(initialState);
+UIContext.displayName = "UIContext";
+
+const uiReducer = (state: UIState, action: Action) => {
+  switch (action.type) {
+    case "OPEN_MODAL":
+      return { ...state, displayModal: true };
+    case "CLOSE_MODAL":
+      return { ...state, displayModal: false };
+    case "SET_MODAL_VIEW":
+      return { ...state, modalView: action.view };
+  }
+};
+
+export const UIProvider: FC = ({ children }, props) => {
+  const [state, dispatch] = useReducer(uiReducer, initialState);
+
+  const openModal = useCallback(
+    () => dispatch({ type: "OPEN_MODAL" }),
+    [dispatch]
+  );
+  const closeModal = useCallback(
+    () => dispatch({ type: "CLOSE_MODAL" }),
+    [dispatch]
+  );
+  const setModalView = useCallback(
+    (view: MODAL_VIEWS) => dispatch({ type: "SET_MODAL_VIEW", view }),
+    [dispatch]
+  );
+
+  const value = useMemo(
+    () => ({ ...state, openModal, closeModal, setModalView }),
+    [state]
+  );
+
+  console.log(value, props);
+
+  return (
+    <UIContext.Provider value={value} {...props}>
+      {children}
+    </UIContext.Provider>
+  );
+};
+
+export const useUI = () => {
+  const context = useContext(UIContext);
+  if (context === undefined) {
+    throw new Error(`useUI must be used within a UIProvider`);
+  }
+  return context;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@/styles/styles.scss";
+
 import type { AppProps } from "next/app";
 import { Head } from "@/components/common/head";
+import { UIProvider } from "@/lib/contexts/ui-context";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
-      <Component {...pageProps} />
+      <UIProvider>
+        <Component {...pageProps} />
+      </UIProvider>
     </>
   );
 }
