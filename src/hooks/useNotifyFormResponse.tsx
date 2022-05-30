7e76343900de09e19c7e718eb04d4bf2077e0903
alxpgv <alex@ipcoder.ru>
2022-05-31 01:35:44 +0300

    upd

--- src/hooks/useNotifyFormResponse.tsx
+++ src/hooks/useNotifyFormResponse.tsx
@@ -6,33 +6,28 @@ export const useNotifyFormResponse = () => {
   const { openNotify, setNotifyView } = useUI();
 
   return (response: FormResponse | null) => {
-    if (response) {
-      // errors on server
-      if (response.status) {
-        setNotifyView("FORM_SEND_SUCCESS");
-        openNotify();
-        return true;
-      } else {
-        // each errors
-        if (response.errors?.length) {
-          setNotifyView(
-            "FORM_SEND_ERROR",
-            response.errors.map((error, index: number) => {
-              const [key, value] = Object.entries(error)[0];
-              return <div key={index}>{`${key}: ${value}`}</div>;
-            })
-          );
-          openNotify();
-        } else {
-          setNotifyView("FORM_SEND_ERROR", errorMessages.sendingError);
-          openNotify();
-        }
-        return false;
-      }
-    } else {
-      setNotifyView("FORM_SEND_ERROR", errorMessages.sendingError);
+    // errors on server
+    if (response?.status) {
+      setNotifyView("FORM_SEND_SUCCESS");
+      openNotify();
+      return true;
+    }
+
+    // each errors
+    if (response?.errors?.length) {
+      setNotifyView(
+        "FORM_SEND_ERROR",
+        response.errors.map((error, index: number) => {
+          const [key, value] = Object.entries(error)[0];
+          return <div key={index}>{`${key}: ${value}`}</div>;
+        })
+      );
       openNotify();
       return false;
     }
+
+    setNotifyView("FORM_SEND_ERROR", errorMessages.sendingError);
+    openNotify();
+    return false;
   };
 };
