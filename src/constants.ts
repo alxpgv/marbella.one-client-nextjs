ab3da7ac41a94f8fda128e3abe9f11c9febf36ae
alxpgv <alex@ipcoder.ru>
2022-05-31 00:57:11 +0300

    moved

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,6 @@ export const errorMessages = {
   required: "Required to complete",
   notValid: "Not valid",
   agree: "Must accept the conditions",
+  sendingError:
+    "Repeat your request again or let us know about the error, thank you.",
 };
